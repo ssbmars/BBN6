1d19a225d442851f44a69b81f6154d8cd204fded
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-03-31 20:50:38 -0700

    host/client normalization for windrack & thunder
    These moves will now work consistently regardless of who is regarded as host/client:
    
    When Windrack is used point blank against someone who has a barrier, the wind gusts will also apply.
    
    When Thunder/BDT is on the same panel as the character that it's targeting, it will always move in the direction that is considered "forward" by the character who spawned it.

---
+++ asm/bn6f_sym.asm
@@ -0,0 +1,12 @@
+
+.definelabel InputRoutineHook, 0x0801FF08
+	.sym off
+.definelabel WindRFirstMove, 0x080CD3A2
+	.sym on
+.definelabel RedirectWindrack, 0x080CD300
+.definelabel WindRStep1, 0x080CD310
+.definelabel WindRStep2, 0x080CD3B0
+.definelabel DeleteSpell, 0x08016C8A
+
+.definelabel ThunderHook, 0x080C956C
+
---
+++ asm/bn6g_sym.asm
@@ -0,0 +1,12 @@
+
+.definelabel InputRoutineHook, 0x0801FF08
+	.sym off
+.definelabel WindRFirstMove, 0x080CEC12
+	.sym on
+.definelabel RedirectWindrack, 0x080CEB70
+.definelabel WindRStep1, 0x080CEB80
+.definelabel WindRStep2, 0x080CEC20
+.definelabel DeleteSpell, 0x08016C8A
+
+.definelabel ThunderHook, 0x080CADDC
+
