5f31ef3b48324c95688b1603542d44cd1e0686c6
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-03-31 20:50:14 -0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-armips bbn6.asm -equ IS_BN6F 1 -equ IS_BN6G 0 -equ IS_EXE6F 0 -equ IS_EXE6G 0 -sym output\output_bn6f.sym
-armips bbn6.asm -equ IS_BN6F 0 -equ IS_BN6G 1 -equ IS_EXE6F 0 -equ IS_EXE6G 0 -sym output\output_bn6g.sym
+armips bbn6.asm -equ VERSION 1 -equ AM_DEBUGGING 0 -sym output\output_bn6f.sym
+armips bbn6.asm -equ VERSION 2 -equ AM_DEBUGGING 0 -sym output\output_bn6g.sym
 
 timeout 3
