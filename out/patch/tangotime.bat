b35555a7672fec38822186b7986c1d7c1501c664
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-04-26 20:27:43 -0700

    add faster formatting for submitting tango patches

---
+++ out/patch/tangotime.bat
@@ -0,0 +1,6 @@
+@echo off
+set /p name="version name [x.y.z] : "
+@echo PRESS F
+xcopy /v /y "bbn6f.bps" tango_patches\bn6f_bingus\v%name%.bps
+xcopy /v /y "bbn6g.bps" tango_patches\bn6g_bingus\v%name%.bps
+timeout 2
