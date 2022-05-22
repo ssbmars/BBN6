b9bd5d9e7b2e4691ba1267bc6ff6cfef86f6d00f
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-05-21 23:11:04 -0700

    Update tangotime.bat

--- out/patch/tangotime.bat
+++ out/patch/tangotime.bat
@@ -3,4 +3,6 @@ set /p name="version name [x.y.z] : "
 @echo PRESS F
 xcopy /v /y "bbn6f.bps" tango_patches\bn6f_bingus\v%name%.bps
 xcopy /v /y "bbn6g.bps" tango_patches\bn6g_bingus\v%name%.bps
+xcopy /v /y "bexe6f.bps" tango_patches\exe6f_bingus\v%name%.bps
+xcopy /v /y "bexe6g.bps" tango_patches\exe6g_bingus\v%name%.bps
 timeout 2
