211dfbc51826eb9440580a5c88fb43f91e7a9aec
ssbmars <46971717+ssbmars@users.noreply.github.com>
2022-07-15 15:39:25 -0700

    update patch output structure

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 @echo off
-armips build.asm -strequ GameName bn6f -equ AM_DEBUGGING 0 -sym out\output_bn6f.sym
-armips build.asm -strequ GameName bn6g -equ AM_DEBUGGING 0 -sym out\output_bn6g.sym
-armips build.asm -strequ GameName exe6f -equ AM_DEBUGGING 0 -sym out\output_exe6f.sym
-armips build.asm -strequ GameName exe6g -equ AM_DEBUGGING 0 -sym out\output_exe6g.sym
+armips build.asm -strequ GameName bn6f -equ AM_DEBUGGING 0 -sym out/output_bn6f.sym
+armips build.asm -strequ GameName bn6g -equ AM_DEBUGGING 0 -sym out/output_bn6g.sym
+armips build.asm -strequ GameName exe6f -equ AM_DEBUGGING 0 -sym out/output_exe6f.sym
+armips build.asm -strequ GameName exe6g -equ AM_DEBUGGING 0 -sym out/output_exe6g.sym
 
-flips -c -b "rom\bn6f.gba" "out\output_bn6f.gba" "out/patch/bbn6f.bps"
-flips -c -b "rom\bn6g.gba" "out\output_bn6g.gba" "out/patch/bbn6g.bps"
-flips -c -b "rom\exe6f.gba" "out\output_exe6f.gba" "out/patch/bexe6f.bps"
-flips -c -b "rom\exe6g.gba" "out\output_exe6g.gba" "out/patch/bexe6g.bps"
+flips -c -b "rom/bn6f.gba" "out/output_bn6f.gba" "out/patch/MEGAMAN6_FXXBR6E_00.bps"
+flips -c -b "rom/bn6g.gba" "out/output_bn6g.gba" "out/patch/MEGAMAN6_GXXBR5E_00.bps"
+flips -c -b "rom/exe6f.gba" "out/output_exe6f.gba" "out/patch/ROCKEXE6_RXXBR6J_00.bps"
+flips -c -b "rom/exe6g.gba" "out/output_exe6g.gba" "out/patch/ROCKEXE6_GXXBR5J_00.bps"
 
 timeout 3
--- out/patch/tangotime.bat
+++ out/patch/tangotime.bat
@@ -1,8 +1,10 @@
 @echo off
 set /p name="version name [x.y.z] : "
-@echo PRESS F
-xcopy /v /y "bbn6f.bps" tango_patches\bn6f_bingus\v%name%.bps
-xcopy /v /y "bbn6g.bps" tango_patches\bn6g_bingus\v%name%.bps
-xcopy /v /y "bexe6f.bps" tango_patches\exe6f_bingus\v%name%.bps
-xcopy /v /y "bexe6g.bps" tango_patches\exe6g_bingus\v%name%.bps
-timeout 2
+
+xcopy /v /y "MEGAMAN6_FXXBR6E_00.bps" tango_patches\bn6_bingus\v%name%\
+xcopy /v /y "MEGAMAN6_GXXBR5E_00.bps" tango_patches\bn6_bingus\v%name%\
+xcopy /v /y "ROCKEXE6_RXXBR6J_00.bps" tango_patches\exe6_bingus\v%name%\
+xcopy /v /y "ROCKEXE6_GXXBR5J_00.bps" tango_patches\exe6_bingus\v%name%\
+
+
+timeout 3
